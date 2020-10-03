a617a892d298cd0d121904818841df7d02d73f5f
Rudranil Maity <48630058+Pegasus-01@users.noreply.github.com>
2020-10-03 12:27:50 +0530

    Add files via upload

---
+++ 01-variables.js
@@ -0,0 +1,13 @@
+var fullname = "Rudranil Maity";
+
+var coursename = "React";
+var isloggedin = true;
+var loggedcount = 34;
+  
+console.log(loggedcount);
+
+var payment_mode;
+
+payment_mode = "credit card";
+
+console.log(payment_mode);
