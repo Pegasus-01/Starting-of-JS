8079e69e8e3192cace945b0786e6e5407c22bc6e
Rudranil Maity <48630058+Pegasus-01@users.noreply.github.com>
2020-10-04 12:43:48 +0530

    Update and rename 02-basic_user_login.js to 02-basic_user_signup.js

--- 02-basic_user_signup.js
+++ 02-basic_user_signup.js
@@ -7,12 +7,9 @@ var confirmPassword = "123456";
 var courseCount = 0;
 var isLoggedInFromGoogle = false;
 
-// fullname = prompt("Enter your name");
-
 console.log(uid);
 console.log("Full Name is: ", fullName);
 console.log(email);
-
 console.log(`
     With Unique ID: ${uid}
     User is : ${fullName}
