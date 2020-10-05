68d3c047e343c7be43449c3314472ef609f0294d
Rudranil Maity <48630058+Pegasus-01@users.noreply.github.com>
2020-10-05 22:41:04 +0530

    Update 10_Coercion_&_FalsyValues_lyst1171.js

--- 10_Coercion_&_FalsyValues_lyst1171.js
+++ 10_Coercion_&_FalsyValues_lyst1171.js
@@ -12,3 +12,8 @@ if (2 === user) {
 }
 
 // console.log("2" + "2");
+// if you write
+//console.log('5'-5)   --> the output will be "0"
+//and if you write the code as ...
+//console.log('5' + 5)   -->then the output will be "55"
+//this is the core concept of coercion using global contexts.
